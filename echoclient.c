3bd0e4c3221329d9cce2ce4059dd1ca6561f2d0b
sk9028 <sl9028@naver.com>
2019-10-10 18:51:42 +0900

    과제 5-3-2 완성

--- echoclient.c
+++ echoclient.c
@@ -14,13 +14,13 @@ int main(){
 	char rcvBuffer[BUFSIZE];//서버에서 보내준 메세지를 저장하는 변수
 	char sendbuffer[BUFSIZE]="안녕하세요";
 	char *str;
-	char strr[BUFSIZE]="the little prince";
 	char *ptr;
 	char *ptrr;
 	char *ptr1;
 	char *ptr2;
 	char *ptr3;
-	char *ptr4;
+
+
 	//1. 클라이언트 소켓 생성
 	c_socket = socket(PF_INET, SOCK_STREAM, 0); //서버와 동일한 설정으로 생성
 	//2.소켓 정보 초기화
@@ -43,15 +43,18 @@ int main(){
 		//5. 서버에서 보낸 메시지 읽기 
 		n = read(c_socket, rcvBuffer, sizeof(rcvBuffer));
 		strcpy(str,rcvBuffer);
-		ptr=str+7;   //strcmp delete
-//		ptrr=str+13;
-		//ptrr=strtok(ptr,"  ");
-		ptr1=strtok(ptr," ");  			//strcmp [hello] hello
-		ptr2=strtok(ptr," ");
-		           //strcmp hello [hello]
-		
-		
-		
+		ptr=str;   						//서버로 부터 메시지를 주고 받아와서 ptr에 저장
+		ptr1=strtok(ptr," ");  			//[strcmp] hello hello 
+		ptr=str+7;							//strcmp 자르기
+		ptr2=strtok(ptr," ");			//strcmp [hello] hello
+		ptr3=strtok(NULL," ");			//strcmp hello [hello]
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		//./client 후 strcmp hello hello를 입력할 때 마지막에 공백을 해주셔야 정확한 값이 출력됩니다!
+		//./client -> "strcmp[공백]hello[공백]hello[공백]"		
+		// 이 점 참고해서 실행 해주시면 감사하겠습니다.
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 		//서버에서 보내준 메세지를 rcvBuffer에 저장하고, 메세지의 길이를 리턴
 		//만약 read에 실패하면, -1을 리턴
 		if (n < 0){ //read() 함수 실패 
@@ -59,14 +62,21 @@ int main(){
 			return -1;
 		}
 		rcvBuffer[n] = '\0'; //문자열 뒷부분 깨짐 방지
-		//printf("길이 : %s \n",ptr);   //str+8=ello hello  ,, str+13=hello
-		printf("first value : %s \n",ptr1);   //str+8=ello hello  ,, str+13=hello
-		printf("second value : %s \n",ptr2);   //str+8=ello hello  ,, str+13=hello
-		if(ptr1==ptr2){
-			printf("two value == %d\n",strcmp(ptr1,ptr2));
+		
+		
+		printf("first 길이 : %d \n",strlen(ptr1));  //strcmp 자른 후 길이 출력
+		printf("second 길이 : %d \n",strlen(ptr2)); //첫번째 문자열 길이 출력
+		printf("third 길이 : %d \n",strlen(ptr3));  //두번째 물자열 길이 출력
+
+		
+		printf("first 문자열 출력 : %s \n",ptr2);   //첫번째 문자열 출력
+		printf("second 문자열 출력 : %s \n",ptr3);   //두번째 문자열 출력
+		if(strcmp(ptr2,ptr3)){
+		
+			printf("두 문자열은 같지 않습니다. == 1 \n");  
 		}
 		else{
-			printf("two value == 1 n");
+			printf("두 문자열은 같습니다. == 0 \n");
 		}
 		
 		close(c_socket);
